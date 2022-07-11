01875ce5e51c6c7d09b80f0be86c5dbd2944bbec
laceyhan <laceyhan@student.21-school.ru>
2022-07-11 19:31:42 +0300

    some

--- src/code/main.c
+++ src/code/main.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv) { return 0; }
+int main() {
+    // int n = 333;
+    char* alone = NULL;
+    // alone = (char*)malloc(n * sizeof(char));
+
+    printf("%s\n", alone);
+
+    return 0;
+}
